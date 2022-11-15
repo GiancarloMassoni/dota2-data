0eb32fee9d9e7206e79f037304e5ea3eafb372c5
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-11-15 18:31:29 +0000

    Fixed variable names for rows

--- js/main.js
+++ js/main.js
@@ -16,21 +16,21 @@ function getSteamProfile(id) {
     var $profileImg = document.querySelector('.profile-img');
     $profileImg.setAttribute('src', xhr.response.profile.avatarfull);
 
-    var $row1 = document.querySelector('.profile-table-row1');
-    $row1.textContent = xhr.response.profile.personaname;
+    var $rowUserName = document.querySelector('.username-row');
+    $rowUserName.textContent = xhr.response.profile.personaname;
 
-    var $row2 = document.querySelector('.profile-table-row2');
-    $row2.textContent = 'Estimated MMR: ' + xhr.response.mmr_estimate.estimate;
+    var $rowMmr = document.querySelector('.mmr-row');
+    $rowMmr.textContent = 'Estimated MMR: ' + xhr.response.mmr_estimate.estimate;
 
-    var $row3 = document.querySelector('.profile-table-row3');
-    $row3.textContent = 'Rank: ' + xhr.response.rank_tier;
+    var $rowRank = document.querySelector('.rank-row');
+    $rowRank.textContent = 'Rank: ' + xhr.response.rank_tier;
 
-    var $row7 = document.querySelector('.profile-table-row7');
+    var $rowProfileLink = document.querySelector('.profile-link-row');
 
     var $a = document.createElement('a');
     $a.setAttribute('href', xhr.response.profile.profileurl);
     $a.textContent = 'Link to Steam Profile';
-    $row7.appendChild($a);
+    $rowProfileLink.appendChild($a);
 
   });
 

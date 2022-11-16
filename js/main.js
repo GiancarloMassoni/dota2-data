18e4286bf0d2ab8cb139921e09b17528ac56e59e
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-11-16 00:43:04 +0000

    added recent matches table and css

--- js/main.js
+++ js/main.js
@@ -3,10 +3,13 @@ var $profile = document.querySelector('.profile');
 var $homeView = document.querySelector('.home-view');
 var $profileView = document.querySelector('.profile-view');
 var $dotaData = document.querySelector('.dota-data');
+var $matchView = document.querySelector('.match-view');
+var $matches = document.querySelector('.matches');
 
 $dotaData.addEventListener('click', swapHomeView);
 $profile.addEventListener('click', swapProfileView);
 $form.addEventListener('submit', idSubmit);
+$matches.addEventListener('click', swapMatchView);
 
 function getSteamProfile(id) {
 
@@ -65,20 +68,86 @@ function idSubmit(event) {
   data.id = $form.elements.steamid.value;
   getSteamProfile(data.id);
   getWinLosses(data.id);
+  getMatches(data.id);
   $homeView.className = 'home-view hidden';
   $profileView.className = 'profile-view';
   data.view = 'profile';
 
 }
 
+function getMatches(id) {
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.opendota.com/api/players/' + id + '/recentMatches');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+
+    var $table = document.querySelector('.table-matches');
+
+    for (var i = 0; i < xhr.response.length; i++) {
+      var $tableRow = document.createElement('tr');
+      $tableRow.setAttribute('data-id', xhr.response[i].hero_id);
+      $table.appendChild($tableRow);
+
+      var $heroImgCol = document.createElement('td');
+      $tableRow.appendChild($heroImgCol);
+
+      var $heroImg = document.createElement('img');
+      $heroImgCol.appendChild($heroImg);
+
+      var $heroCol = document.createElement('td');
+      $heroCol.textContent = xhr.response[i].hero_id;
+      $tableRow.appendChild($heroCol);
+
+      var $resultCol = document.createElement('td');
+      if (xhr.response[i].radiant_win === true) {
+        $resultCol.textContent = 'Radiant Win';
+      } else {
+        $resultCol.textContent = 'Dire Win';
+      }
+      $tableRow.appendChild($resultCol);
+
+      var $typeCol = document.createElement('td');
+      if (xhr.response[i].game_mode === 22) {
+        $typeCol.textContent = 'All Pick';
+      } else {
+        $typeCol.textContent = 'Custom Game';
+      }
+      $tableRow.appendChild($typeCol);
+
+      var $durationCol = document.createElement('td');
+      var duration = xhr.response[i].duration / 60;
+      var difference = duration - Math.floor(duration);
+      var seconds = 60 * difference;
+
+      $durationCol.textContent = Math.floor(duration) + ':' + seconds.toFixed(0);
+      $tableRow.appendChild($durationCol);
+
+      var $kdaCol = document.createElement('td');
+      $kdaCol.textContent = xhr.response[i].kills + '/' + xhr.response[i].deaths + '/' + xhr.response[i].assists;
+      $tableRow.appendChild($kdaCol);
+    }
+  });
+  xhr.send();
+}
+
 function swapHomeView(event) {
   $homeView.className = 'home-view';
   $profileView.className = 'profile-view hidden';
+  $matchView.className = 'match-view hidden';
   data.view = 'home';
 }
 
 function swapProfileView(event) {
   $homeView.className = 'home-view hidden';
   $profileView.className = 'profile-view';
+  $matchView.className = 'match-view hidden';
   data.view = 'profile';
+}
+
+function swapMatchView(event) {
+  $homeView.className = 'home-view hidden';
+  $profileView.className = 'profile-view hidden';
+  $matchView.className = 'match-view';
+  data.view = 'match';
 }
